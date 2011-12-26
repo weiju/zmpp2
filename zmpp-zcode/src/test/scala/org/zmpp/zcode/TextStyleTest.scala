05412d344362d9d9bec77f2f5711e93b8127b27e
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-12-26 09:51:49 -0800

    added test class for text style conversion

---
+++ zmpp-zcode/src/test/scala/org/zmpp/zcode/TextStyleTest.scala
@@ -0,0 +1,51 @@
+/*
+ * Created on 2011/10/05
+ * Copyright (c) 2010-2011, Wei-ju Wu.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of Wei-ju Wu nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.zmpp.zcode
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TextStyleSpec extends FlatSpec with ShouldMatchers {
+
+  "TextStyle" should "encode style, font and foreground colors" in {
+    val mask = TextStyles.makeStyle(6, 2, 5, 7)
+    mask should be (0x5726)
+  }
+  it should "decode a mask value" in {
+    TextStyles.isRoman(0x5720) should be (true)
+    TextStyles.isBold(0x5726) should be (true)
+    TextStyles.isItalic(0x5726) should be (true)
+    TextStyles.fontNumber(0x5726) should be (2)
+    TextStyles.foregroundColor(0x5726) should be (5)
+    TextStyles.backgroundColor(0x5726) should be (7)
+  }
+}
