c30466f4edfa3eea8c20560372407879487300d3
Wei-ju Wu <Weiju.Wu@gmail.com>
2012-02-20 09:47:22 -0800

    migrated GlkDefs classes to Java

---
+++ zmpp-glk/src/main/java/org/zmpp/glk/GestaltSelector.java
@@ -0,0 +1,57 @@
+/*
+ * Created on 2012/02/20
+ * Copyright (c) 2010-2012, Wei-ju Wu.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of Wei-ju Wu nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.zmpp.glk;
+
+public final class GestaltSelector {
+    public static final int Version              = 0;
+    public static final int CharInput            = 1;
+    public static final int LineInput            = 2;
+    public static final int CharOutput           = 3;
+    public static final int MouseInput           = 4;
+    public static final int Timer                = 5;
+    public static final int Graphics             = 6;
+    public static final int DrawImage            = 7;
+    public static final int Sound                = 8;
+    public static final int SoundVolume          = 9;
+    public static final int SoundNotify          = 10;
+    public static final int Hyperlinks           = 11;
+    public static final int HyperlinkInput       = 12;
+    public static final int SoundMusic           = 13;
+    public static final int GraphicsTransparency = 14;
+    public static final int Unicode              = 15;
+    /*
+    public static final String[] Names = {
+        "Version", "CharInput", "LineInput", "CharOutput",
+        "MouseInput", "Timer", "Graphics", "DrawImage",
+        "Sound", "SoundVolume", "SoundNotify", "Hyperlinks",
+        "HyperlinkInput", "SoundMusic", "GraphicsTransparency",
+        "Unicode"
+        };*/
+}
+
---
+++ zmpp-glk/src/main/java/org/zmpp/glk/GlkWindowPosition.java
@@ -0,0 +1,51 @@
+/*
+ * Created on 2012/02/20
+ * Copyright (c) 2010-2012, Wei-ju Wu.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of Wei-ju Wu nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.zmpp.glk;
+
+/**
+ * Window positioning constants (part of method bit mask).
+ */
+public final class GlkWindowPosition {
+    public static final int Left         = 0x00;
+    public static final int Right        = 0x01;
+    public static final int Above        = 0x02;
+    public static final int Below        = 0x03;
+    public static final int Mask         = 0x0f;
+  
+    public static String name(int method) {
+        switch (method & Mask) {
+        case Left:  return "Left";
+        case Right: return "Right";
+        case Above: return "Above";
+        case Below: return "Below";
+        default: throw new IllegalArgumentException(String.format("unknown window position: %d",
+                                                                  method & Mask));
+        }
+    }
+}
